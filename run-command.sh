03b319771b5e94f7293d73636ceded73cb12d910
Jeffrey Olsen <jeffrey.olsen@dell.com>
2017-05-16 11:13:09 -0400

    Update run-command.sh

--- run-command.sh
+++ run-command.sh
@@ -1,4 +1,4 @@
 docker volume create --name concourse-db
 docker volume create --name concourse-web-keys
 docker volume create --name concourse-worker-keys
-DOCKERHOST=192.168.56.51 docker-compose up
+DOCKERHOST=127.0.0.1 docker-compose up -d
